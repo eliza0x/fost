993aa0e92b89a656aebcf2d38a50700485816690
eliza0x <me@eliza.link>
2018-01-25 06:19:57 +0900

    Update: ldiでは符号拡張を行わないようにした

--- src/Decode.sv
+++ src/Decode.sv
@@ -375,7 +375,7 @@ module Decode(
         do_jump       = 0;
         is_branch     = 0;
         jump_address  = 0;
-        `define h from_inst.inst[rs_begin]
+        `define h 0
         val1 = {`h,`h,`h,`h,`h,`h,`h,`h,
                 from_inst.inst[rs_begin:rt_end]};
         `undef h
